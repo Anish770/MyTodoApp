c8aacd43052254fe7b26f8290d4e685002e4a458
Anish Chettri <113011778+Anish770@users.noreply.github.com>
2024-07-26 11:59:28 +0530

    additonals

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        "frost-start":"#000428",
+        "frost-end":"#004e92"
+      }
+    },
+  },
+  plugins: [],
+}
